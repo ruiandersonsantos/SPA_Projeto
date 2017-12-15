a4d9ff863382492f98996de1c13ef75733c48555
Rui Anderson <ruiandersonsantos@gmail.com>
2017-12-15 13:28:54 -0200

    Modificação visual code

--- src/app/empresa/empresa-edit.component.ts
+++ src/app/empresa/empresa-edit.component.ts
@@ -27,7 +27,7 @@ export class EmpresaEditComponent implements OnInit {
       false
   );
 
-  // construtor modificado no phpstorm
+  // construtor modificado no visual code
   constructor(
       private activatedRoute: ActivatedRoute,
       private empresaservice: EmpresaService,
